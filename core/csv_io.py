94a16585ab62bbe38d214c2dd02c744aecb3e9c0
Thais-Alvim <thalvim@gmail.com>
2025-10-05 20:40:51 -0300

    feat: agrega archivo csv.io con lectura y procesamiento de datos

--- core/csv_io.py
+++ core/csv_io.py
@@ -1,1 +1,26 @@
-#Ler e validar o arquivo CSV
+import csv
+#Lee el archivo CSV y genera una lista de diccionarios con los países.
+def cargar_paises(ruta: str) -> list[dict]:
+
+    paises = []
+
+    with open(ruta, newline="", encoding="utf-8") as f:
+        lector = csv.reader(f)
+        columnas = next(lector) 
+        columnas = [i.lower().strip() for i in columnas]
+        
+        # Lecturas de las lineas
+        for i, fila in enumerate(lector, start=2):
+            #Manejo de errores(try-except)
+            try:
+                nombre, superficie, poblacion, continente = [x.strip() for x in fila]
+
+                paises.append({
+                    "Nombre": nombre,
+                    "Superficie": int(superficie),
+                    "Población": int(poblacion),
+                    "Continente": continente
+                })
+            except Exception:
+                print(f"Línea {i}: error en el formato de datos")
+    return paises
